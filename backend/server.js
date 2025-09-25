23db37272499f523ad3f8f1cff192cee6d45c1c5
Sunny Kumar <sunny9102003kumar@gmail.com>
2025-09-25 15:43:06 -0400

    Backend Server is running

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,20 @@
 import express from 'express'
+import cors from "cors"
 
-const app = express()
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("Backend is running.")
+})
+
+const PORT = 3000
+
+app.listen(PORT,  () => {
+    console.log("Waste Watch Server is running.")
+})
+
+export default app;
