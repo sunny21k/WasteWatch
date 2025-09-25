aa171a269fc3bab3226a1652282a29decf00d016
Sunny Kumar <sunny9102003kumar@gmail.com>
2025-09-25 02:12:04 -0400

    fixed rewards section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   const hideLayout = location.pathname === "/login-signin"
 
   return (
-    <div className='flex flex-col min-h-screen bg-green-50'>
+    <div className='flex flex-col min-h-screen '>
     {!hideLayout && <Navbar />}
 
     <main className='flex-grow'>
--- frontend/src/pages/Rewards.jsx
+++ frontend/src/pages/Rewards.jsx
@@ -5,11 +5,17 @@ const Rewards = () => {
   const [rewards, setRewards] = useState(100)
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen px-6'>
-      <h1 className='text-4xl sm:text-5xl font-bold text-green-900 mb-6 text-center'>Your Rewards</h1>
+    <div className='flex flex-col items-center justify-start pt-20 px-6'>
 
-      <div className='flex flex-col items-center justify-center bg-white shadow-lg rounded-2xl p-8 w-72 sm:w-96 
-        border-b-3 border-b-green-800'>
+      {/* Rewards Header Section */}
+      <div className='text-center mb-10'>
+        <h1 className='text-4xl sm:text-5xl font-bold text-green-900 mb-4'>Your Rewards Dashboard</h1>
+        <p className='text-lg sm:text-xl text-primary-gray'>See your points, achievements, and how you're making a difference in your community.</p>
+      </div>
+
+      {/* Rewards Card */}
+      <div className='`flex flex-col items-center justify-center bg-white shadow-lg rounded-2xl p-8 w-72 sm:w-96 
+        border-b-3 my-10 border-b-green-800'>
         <div className='flex items-center gap-3 text-green-700 text-3xl sm:text-4xl font-bold'>
           <RiCoinsLine className='w-10 h-10'/>
           <span>{rewards}</span>
