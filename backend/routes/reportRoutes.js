9cb51e035e5425d30b9548078b8b6145a93aaac4
Sunny Kumar <sunny9102003kumar@gmail.com>
2025-11-16 16:10:41 -0500

    added routes

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { createReport } from "../controllers/reportController.js";
+import { createReport, getMyReports, getAllReports } from "../controllers/reportController.js";
 import upload from "../middleware/multer.js";
 import { protect } from "../middleware/auth.js";
  
 const reportRouter = express.Router();
 
 reportRouter.post("/create", protect, upload.single('image'), createReport)
+reportRouter.get("/all-reports", getAllReports)
+reportRouter.get("/my-reports", protect, getMyReports);
+
 
 export default reportRouter;
